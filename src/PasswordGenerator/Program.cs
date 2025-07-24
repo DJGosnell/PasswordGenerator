d2513bc55183af7702ab5d2c5f605b37cee76ef8
djgosnell <djgosnell@gmail.com>
2025-07-24 17:20:50 -0400

    Removal of old solution. Simplification of password generation for most common scenario.

--- src/PasswordGenerator/Program.cs
+++ src/PasswordGenerator/Program.cs
@@ -4,20 +4,29 @@ using PasswordGenerator;
 
 Console.WriteLine("Password Generator");
 
-if (!InputHelper.PromptInput("Mode (1 = Spaces in Password, 2 = No Spaces)", 2, out var mode)
-    || !InputHelper.PromptInput("Use one special character", true, out var specialCharacter)
-    || !InputHelper.PromptInput("Set one word as uppercase", true, out var uppercaseOneWord)
-    || !InputHelper.PromptInput("Complexity of words used (1 least complex - 18 most complex)", 2, out var levelOfWords)
-    || !InputHelper.PromptInput("Spaces between words", true, out var spacesBetweenWords)
-    || !InputHelper.PromptInput("Number of words", 3, out var words)
-    ||!InputHelper.PromptInput("Number of numbers", 2, out var numbers))
+if (!InputHelper.PromptInput("Mode (1 = Spaces in Password, 2 = No Spaces)", 2, out var mode))
     return;
+
+bool? specialCharacter = true;
+bool? uppercaseOneWord = true;
+int? levelOfWords = 2;
+int? words = 3;
+int? numbers = 2;
+if (mode.Value == 1)
+{
+    if (!InputHelper.PromptInput("Use one special character", specialCharacter.Value, out specialCharacter)
+        || !InputHelper.PromptInput("Set one word as uppercase", uppercaseOneWord.Value, out uppercaseOneWord)
+        || !InputHelper.PromptInput("Complexity of words used (1 least complex - 18 most complex)", levelOfWords.Value, out levelOfWords)
+        || !InputHelper.PromptInput("Number of words", words.Value, out words)
+        || !InputHelper.PromptInput("Number of numbers", numbers.Value, out numbers))
+        return;
+}
 
 var sw = Stopwatch.StartNew();
 Console.Write("Parsing Words... ");
 var wordLines = Math.Min(172681, levelOfWords.Value * 10000);
 char[] specialCharacters = ['~','!','@','#','$','%','^','&','*'];
-var generator = new Generator(wordLines, words.Value, numbers.Value, spacesBetweenWords.Value, specialCharacter.Value, specialCharacters, uppercaseOneWord.Value);
+var generator = new Generator(wordLines, words.Value, numbers.Value, mode.Value == 1, specialCharacter.Value, specialCharacters, uppercaseOneWord.Value);
 generator.ParseWords();
 Console.WriteLine($"Completed in {sw.ElapsedMilliseconds}ms.");
 
